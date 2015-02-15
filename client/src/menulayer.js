3817f54b5e9df2d5efd0888e14583cb9b78f0292
Michael Schmidt <lavisrap@gmail.com>
2015-02-15 12:32:54 +0200

    Gate is rolling in.

--- client/src/menulayer.js
+++ client/src/menulayer.js
@@ -17,6 +17,7 @@ var MenuLayer = cc.Layer.extend({
 		
         var size = cc.winSize;
 		var items = [];
+		var gate;
 		
 		for( var i=0 ; i<labelsAndCallbacks.length ; i++ ) {
 			var frame = cc.spriteFrameCache.getSpriteFrame("item"+(i+1)),
@@ -25,7 +26,7 @@ var MenuLayer = cc.Layer.extend({
 	    		spritesDisabled =cc.Sprite.create(frame,cc.rect(0,0,380,100));
 
 			var label = cc.LabelBMFont.create( labelsAndCallbacks[i].label , "res/fonts/amtype36.fnt" , cc.LabelAutomaticWidth, cc.TEXT_ALIGNMENT_CENTER, cc.p(0, 0) );
-			label.setPosition(cc.p(190,40));
+			label.setPosition(cc.p(190,label.getContentSize().height));
 			label.setColor(cc.color(122,80,77,155));
 			spritesNormal.addChild(label, 5);	
 			//spritesSelected.addChild(label, 5);	
@@ -43,16 +44,29 @@ var MenuLayer = cc.Layer.extend({
 			ch[i].y = ch[i].y + BEES_MENU_Y_OFFSETS[i];
 		}
 		
-        this.addChild(menu);
+        this.addChild(menu,1);
     	menu.setPosition(cc.p(size.width * 1.1,size.height/2));
 	    menu.setScale(0.1);
 
-		menu.runAction(cc.EaseElasticOut.create(
-			cc.spawn(
-				cc.scaleTo(2.5,0.85,0.85),
-				cc.moveTo(2.5,570,200)
+		menu.runAction(cc.sequence(
+			cc.delayTime(0.33),
+			cc.EaseElasticOut.create(
+				cc.spawn(
+					cc.scaleTo(2.5,1,1),
+					cc.moveTo(2.5,930,250)
+				)
 			)
 		));
+
+		// creating gate		
+		this.gate = cc.Sprite.create(cc.spriteFrameCache.getSpriteFrame("gate"),cc.rect(0,0,1136,640)),
+		this.gate.setPosition(cc.p(1136*1.5,370));
+		this.gate.setScale(1.15);
+		
+		this.gate.runAction(cc.EaseSineOut.create(
+			cc.moveTo(1,568,360)
+		));
+        this.addChild(this.gate,0);
 
         return true;
     }
